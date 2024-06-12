4b3086b77879fefc502e3b100d67c2268b7967b8
Daniel Vega <danivega@umich.edu>
2024-06-12 09:04:31 -0500

    testing sending algorithm file

---
+++ lib/algorithm/algorithm.dart
@@ -0,0 +1,66 @@
+/*1.) Finding prerequisite chain
+
+
+//2.) Finding remaining credits needed
+modifies class variables for remaining needed requirements, ex 12 flex techs remaining, 16 IB remaining etc
+
+
+//3.) Building schedule with remaining credits needed
+returns list of classes that need to be taken, in any order. add in 4 credit chunks requirement credits as a class as a placeholder. (ex. make a class that is just a generic flex tech with 4 credits that is used whenever a flex tech is needed)
+
+
+//4.) Assemble the classes in a usable order
+take the list of classes, and make them into a schedule.
+first add in classes, preceded by prerequisites, then fill in with all of the other random credits (you don't necessarily have to do the steps in that order, just that way of looking at things)
+
+
+
+
+//make requirements generalizable
+ //make a function that get requirements
+ //in the future the major database would come in where we would search
+//have access to a function that gives any class for requirements
+
+
+
+
+*/
+
+
+import 'class.dart';
+class AlgorithmTesting {
+
+
+ //algorithm data
+
+
+
+
+
+
+
+
+ //-------------------------------------------
+ //functions
+ List<Class> getPrereqs(Class class) {
+
+
+ }
+
+
+ List<Class> getProgramRequirements(/*major, minor*/){
+
+
+ }
+
+
+ List<Class> getListOfClasses(/*reqs needed*/) {
+
+
+ }
+
+
+
+
+}
+
