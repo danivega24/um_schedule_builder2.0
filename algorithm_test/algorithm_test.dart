8bb85803d81e4da06565933274972991f1bf0c87
Daniel Vega <danivega@umich.edu>
2024-08-02 21:59:01 -0500

    worked on algorithm_testing, now have to debug

---
+++ algorithm_test/algorithm_test.dart
@@ -0,0 +1,23 @@
+//testing and debugging algorithm.dart
+import '../lib/algorithm/algorithm.dart';
+import '../lib/database/major.dart';
+import '../lib/database/class.dart';
+import '../lib/database/class_database_helper.dart';
+
+final ClassDatabaseHelper _class_database = ClassDatabaseHelper.instance;
+ClassDatabaseHelper get classDatabase {return _class_database; }
+
+void main() async {
+  AlgorithmTesting tester = AlgorithmTesting(16, 16, 16, classDatabase);
+  List<Class> _sequence = await tester.class_sequence();
+  for(int i = 0; i < _sequence.length; i++)
+  {
+    String id = _sequence[i].id.toString();
+    String dept = _sequence[i].department;
+    String number = _sequence[i].courseNumber.toString();
+
+    print('id: $id, dept: $dept, number: $number\n'); 
+  }
+  
+  print('Hello World!');
+}
